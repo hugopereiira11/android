6f1045e5c7d8f504e45401c6d6f5f688289eb54b
hugopereiira11 <hugopereiira11@gmail.com>
2023-12-21 09:13:35 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+const footer = document.querySelector('#footer')
+
+footer.innerHTML += `
+<h1>Prevenção de Perdas</h1>
+<p>Controle de Prevenção e Perdas do MiniBox31</p>
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Trocas</li>
+        <li>NF-e Devolução</li>
+        <li>Orçamentos</li>
+    </ul>
+</nav>
+`
